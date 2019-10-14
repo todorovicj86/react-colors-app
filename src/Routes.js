5797cdec8856ac5f45317cf90a675799292c0fc7
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-10-14 12:10:41 +0200

    Remove onCopy method and copied from the state

--- src/Routes.js
+++ src/Routes.js
@@ -17,12 +17,10 @@ class Routes extends Component {
         super(props);
         this.state = {
             format: "hex",
-            copied: false,
             colorPalettes: JSON.parse(window.localStorage.getItem("palettes") || "[]")
         }
     
         this.handleFormat = this.handleFormat.bind(this)
-        this.onCopy = this.onCopy.bind(this)
         this.removePalette = this.removePalette.bind(this)
         this.savePalette = this.savePalette.bind(this)
       }
@@ -49,11 +47,6 @@ class Routes extends Component {
               format: value,
           })
       }
-      onCopy(value){
-        this.setState({
-          copied: value
-        })
-      }
 
     removePalette(id){
       const oldPalette = [...this.state.colorPalettes];
@@ -73,104 +66,101 @@ class Routes extends Component {
     
   
     render(){
-        const getPalette = props => {
-            let name = props.match.params.name;
-                 
-            let currentPalette = this.state.colorPalettes.find(
-              palette => palette.id === name
-              
-            );
-            return(
-              <Page>
-                <ColorPalette 
-                  {...props} 
-                  palette={getPaletteShades(currentPalette)} 
-                  format={this.state.format} 
-                  handleFormat = {this.handleFormat}
-                  onCopy = {this.onCopy} 
-                  sliderMarks = {this.props.marks}
-                  />
-              </Page>
-            )
-        };
-      
-        const getColorShades = props => {
-            let name = props.match.params.name;
-            let colorName = props.match.params.colorName;
-                     
-            let currentPalette = this.state.colorPalettes.find( 
-              palette => palette.id === name
-            )
-            let currentColor = currentPalette.colors.find(
-              color => color.name === colorName
-              
-            )
-      
-            return( 
-              <Page>
-                <PaletteShades 
-                  {...props} 
-                  color={currentColor} 
-                  palette = {currentPalette} 
-                  format={this.state.format} 
-                  handleFormat = {this.handleFormat}
-                  onCopy = {this.onCopy} 
+      const {colorPalettes, format } = this.state;
+      const getPalette = props => {
+          let name = props.match.params.name;
+                
+          let currentPalette = this.state.colorPalettes.find(
+            palette => palette.id === name
+            
+          );
+          return(
+            <Page>
+              <ColorPalette 
+                {...props} 
+                palette={getPaletteShades(currentPalette)} 
+                format={this.state.format} 
+                handleFormat = {this.handleFormat}
+                sliderMarks = {this.props.marks}
                 />
-              </Page>
-            )
-        }
-
-        const {colorPalettes, format } = this.state;
-
-        return(
-          <Route 
-            render={({location}) => (
-              <TransitionGroup>
-                <CSSTransition classNames="page" timeout={500} key={location.key}>
-                  <Switch location={location}>
-                    <Route 
-                      exact 
-                      path = "/palette/new" 
-                      render= {(routProps) => (
-                        <Page>
-                          <MakeNewPalette 
-                            format={format}
-                            palettes={colorPalettes} 
-                            savePalette={this.savePalette} 
-                            {...routProps}/>
-                        </Page>
-                      )}
-                    />
-                        
-                      
-                    <Route 
-                      exact 
-                      path="/" 
-                      render = {(routProps) => (
-                        <Page>
-                          <ColorPaletteList 
-                            colorPalettes={colorPalettes} 
-                            removePalette = {this.removePalette} 
-                            {...routProps}/>
-                        </Page>
-                      )} 
-                    />
-                    <Route 
-                      exact 
-                      path = "/palette/:name"
-                      render = {getPalette} />
-                    <Route 
-                      exact 
-                      path = '/palette/:name/:colorName' 
-                      render ={getColorShades} />
-                    <Redirect to = "/" />
-                  </Switch>
-                </CSSTransition>
-            </TransitionGroup>
-            )}
-          />
+            </Page>
+          )
+      };
       
-        )
+      const getColorShades = props => {
+          let name = props.match.params.name;
+          let colorName = props.match.params.colorName;
+                    
+          let currentPalette = this.state.colorPalettes.find( 
+            palette => palette.id === name
+          )
+          let currentColor = currentPalette.colors.find(
+            color => color.name === colorName
+            
+          )
+    
+          return( 
+            <Page>
+              <PaletteShades 
+                {...props} 
+                color={currentColor} 
+                palette = {currentPalette} 
+                format={this.state.format} 
+                handleFormat = {this.handleFormat}
+              />
+            </Page>
+          )
+      }
+
+      return(
+        <Route 
+          render={({location}) => (
+            <TransitionGroup>
+              <CSSTransition classNames="page" timeout={500} key={location.key}>
+                <Switch location={location}>
+                  <Route 
+                    exact 
+                    path = "/palette/new" 
+                    render= {(routeProps) => (
+                      <Page>
+                        <MakeNewPalette 
+                          format={format}
+                          palettes={colorPalettes} 
+                          savePalette={this.savePalette} 
+                          {...routeProps}/>
+                      </Page>
+                    )}
+                  />
+                      
+                    
+                  <Route 
+                    exact 
+                    path="/" 
+                    render = {(routeProps) => (
+                      <Page>
+                        <ColorPaletteList 
+                          colorPalettes={colorPalettes} 
+                          removePalette = {this.removePalette} 
+                          {...routeProps}/>
+                      </Page>
+                    )} 
+                  />
+                  <Route 
+                    exact 
+                    path = "/palette/:name"
+                    render = {getPalette} />
+                  <Route 
+                    exact 
+                    path = '/palette/:name/:colorName' 
+                    render ={getColorShades} />
+                  <Redirect to = "/" />
+                </Switch>
+              </CSSTransition>
+          </TransitionGroup>
+          )}
+        />
+    
+      )
     }
 }
 
